7163565d2d9cb4b4b27e8effe5acc741b9806f0e
JojoELITE <jojoelite1014@gmail.com>
2025-02-18 15:00:56 +0100

    add Section Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Page() {
   return (
     <div className="bg-background">
       <Header />
       <main>
-        {/* <Hero /> */}
+        <Hero />
         {/* <MainContent /> */}
       </main>
       <Footer />
