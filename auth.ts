267f372bdbd0791b3331fa8b15d831cb38e10131
elieboulingui <elieboulingui2@gmail.com>
2025-02-18 18:07:15 +0100

    adding a token for the email

--- auth.ts
+++ auth.ts
@@ -1,14 +1,15 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import credentials from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import authConfig from "./auth.config";
-import bcrypt from "bcryptjs"; // assuming bcrypt is used for password hashing
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-     //@ts-ignore
+      //@ts-ignore
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
@@ -16,11 +17,11 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   ...authConfig,
   secret: process.env.AUTH_SECRET,
   pages: {
-    signIn: "/connexion",
-    error: "/error",
+    signIn: "/login", // This is used for credentials login
+    error: "/error", // Custom error page
   },
   providers: [
-    credentials({
+    CredentialsProvider({
       name: "credentials",
       credentials: {
         email: { label: "Email", type: "email" },
@@ -31,28 +32,24 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
           throw new Error("L'email est requis.");
         }
 
-        // Recherche de l'utilisateur par email
         const user = await prisma.user.findUnique({
           where: {
-             //@ts-ignore
+                //@ts-ignore
             email: credentials.email,
           },
         });
 
-        // Si l'utilisateur n'existe pas, on lance une erreur
         if (!user) {
           throw new Error("Email ou Mot de passe incorrect.");
         }
 
-        // Si l'utilisateur existe mais sans mot de passe, se connecter directement
         if (!user.password) {
           return user; // No password set, allow login directly
         }
 
-        // Si un mot de passe est défini, vérifier la correspondance
         if (credentials?.password) {
           const isCorrectPass = await bcrypt.compare(
-            // @ts-ignore
+                //@ts-ignore
             credentials.password,
             user.password
           );
@@ -62,9 +59,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
           }
         }
 
-        // Retourner l'utilisateur si l'authentification est réussie
         return user;
       },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
@@ -78,7 +78,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
-        token.role = user.role; // Assign the role to the JWT
+        token.role = user.role;
       }
       return token;
     },
