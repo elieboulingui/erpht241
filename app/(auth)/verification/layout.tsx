090cb2fd9e462a2e591c57d35350c797aff64561
elieboulingui <elieboulingui2@gmail.com>
2025-02-21 21:27:33 +0100

    sending

--- app/(auth)/verification/layout.tsx
+++ app/(auth)/verification/layout.tsx
@@ -10,13 +10,9 @@ export default async function OrganisationLayout({
   const session = await auth();
 
   // Vérifier si l'utilisateur est ADMIN
-  if (!session?.user || session.user.role !== "ADMIN") {
-    redirect("/login"); // Redirection côté serveur si l'utilisateur n'est pas ADMIN
-  }
 
   // Récupérer l'ID du propriétaire et l'email de l'utilisateur
-  const ownerId = session.user.id;
-  const email = session.user.email;
+
 
   // Passer l'ID et l'email en tant que props aux enfants
   return <>{children}</>;
