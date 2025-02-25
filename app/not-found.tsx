7003fb1f594da0970f7394aa693035e9ed7f604e
elieboulingui <elieboulingui2@gmail.com>
2025-02-25 09:23:40 +0100

    404 and profile image

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 "use client";  // This makes the file a client-side component
 
 import React from "react";
-import I404Png from "@/public/images/ht241.jpg";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
@@ -12,11 +12,11 @@ const home = () => {
 
 const Page404 = () => (
   <div className="nc-Page404">
-    <div className="container relative pt-12 pb-16 lg:pb-20 lg:pt-5 flex justify-center items-center min-h-screen">
+    <div className="container relative pt-12 pb-16 lg:pb-20 lg:pt-5 flex justify-center items-center h-screen">
       {/* HEADER */}
       <header className="text-center max-w-2xl mx-auto space-y-2">
         <div className="flex justify-center">
-          <Image src={I404Png} alt="not-found" />
+          <Image src="/images/ht241.jpg" height={100} width={100} alt="not-found" className="w-44 h-44 mb-4" />  
         </div>
         <span className="block text-sm text-neutral-800 sm:text-base dark:text-neutral-200 tracking-wider font-medium">
           {`THE PAGE YOU WERE LOOKING FOR DOESN'T EXIST.`}{" "}
