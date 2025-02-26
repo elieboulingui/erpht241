5b3fabcf706aec2d6ee9e800e7de01a25a429106
elieboulingui <elieboulingui2@gmail.com>
2025-02-26 20:46:40 +0100

    code

--- app/listingorg/[id]/components/Affiching.tsx
+++ app/listingorg/[id]/components/Affiching.tsx
@@ -1,7 +1,7 @@
-import  DashboardSidebar  from "@/app/listingorg/[id]/components/DashboardSidebar"
-import LeadGeneration from "@/app/listingorg/[id]/components/LeadGeneration"
-import ContactsList from "@/app/listingorg/[id]/components/ListingContact"
-import Darkpeack from "@/app/listingorg/[id]/components/Darkpeaker"
+import DashboardSidebar from "@/components/DashboardSidebar";
+import LeadGeneration from "@/app/listingorg/[id]/components/LeadGeneration";
+import ContactsList from "@/app/listingorg/[id]/components/ListingContact";
+import Darkpeack from "@/app/listingorg/[id]/components/Darkpeaker";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,15 +9,15 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar"
-import React from "react"
-import { IoMdInformationCircleOutline } from "react-icons/io"
+} from "@/components/ui/sidebar";
+import React from "react";
+import { IoMdInformationCircleOutline } from "react-icons/io";
 
 export default function Affiching() {
   return (
@@ -31,24 +31,26 @@ export default function Affiching() {
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                  Overview
-                  </BreadcrumbLink>
+                  <BreadcrumbLink href="#">Overview</BreadcrumbLink>
                 </BreadcrumbItem>
                 {/* <BreadcrumbSeparator className="hidden md:block" /> */}
                 <BreadcrumbItem>
-                  <BreadcrumbPage> <IoMdInformationCircleOutline className="h-4 w-4" color="gray" /></BreadcrumbPage>
+                  <BreadcrumbPage>
+                    {" "}
+                    <IoMdInformationCircleOutline
+                      className="h-4 w-4"
+                      color="gray"
+                    />
+                  </BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
-            
           </div>
-          
         </header>
-        <Darkpeack/>
-        <LeadGeneration/>
-        <ContactsList/>
+        <Darkpeack />
+        <LeadGeneration />
+        <ContactsList />
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
