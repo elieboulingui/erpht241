5e1ea4fd6eeabc866816549612da36b5352dd269
JojoELITE <jojoelite1014@gmail.com>
2025-03-06 13:57:59 +0100

    change Marque Appareil by Marque

--- app/listing-organisation/[id]/produit/marque/page.tsx
+++ app/listing-organisation/[id]/produit/marque/page.tsx
@@ -1,11 +1,11 @@
-import Header from '@/components/Header'
 import React from 'react'
-import { MarqueAppareilHeader } from './composant/MarqueAppareilHeader'
+import { MarqueHeader } from './composant/MarqueHeader'
+
 
 export default function page() {
   return (
     <div>
-        <MarqueAppareilHeader/>
+        <MarqueHeader/>
     </div>
   )
 }
