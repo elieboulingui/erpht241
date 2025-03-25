34c538c8c5ae505cc855fabda1861f6bba4af452
elieboulingui <elieboulingui2@gmail.com>
2025-03-25 18:14:00 +0100

    update filter marque

--- app/listing-organisation/[id]/produit/marque/page.tsx
+++ app/listing-organisation/[id]/produit/marque/page.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { MarqueHeader } from "./components/MarqueHeader";
 import { TableBrandIa } from "./components/Table";
 import DashboardSidebar from "@/components/DashboardSidebar";
 
-export default function page() {
+export default function Page() {
+  const [filters, setFilters] = useState({ name: "", description: "" });
+
+  const handleFilterChange = (filter: { name: string; description: string }) => {
+    setFilters(filter);
+  };
+
   return (
     <div className="flex w-full">
       <div>
         <DashboardSidebar />
       </div>
       <div className="w-full">
-        <MarqueHeader />
-        <TableBrandIa />
+        <MarqueHeader onFilterChange={handleFilterChange} />
+        <TableBrandIa filters={filters} />
       </div>
     </div>
   );
