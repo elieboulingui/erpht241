0cba06c340eeef468e2553cb6d4b2f0c60b59239
JojoELITE <jojoelite1014@gmail.com>
2025-04-15 04:40:38 +0100

    ADAPTATION HEADER AND tabs EQUIPE

--- app/listing-organisation/[id]/settings/equipe/page.tsx
+++ app/listing-organisation/[id]/settings/equipe/page.tsx
@@ -1,21 +1,19 @@
 import DashboardSidebar from "@/components/DashboardSidebar"
-import { EquipeHeader } from "./components/EquipeHeader"
 import FullTabs from "./components/FullTabs"
 
 function Page() {
 
- return (
-           <div className="flex w-full">
-              <div>
-                <DashboardSidebar />
-              </div>
-        
-              <div className="w-full">
-                <EquipeHeader />
-                <FullTabs />
-            </div>
-          </div>
-    )
+  return (
+    <div className="flex w-full">
+      <div>
+        <DashboardSidebar />
+      </div>
+
+      <div className="w-full">
+        <FullTabs />
+      </div>
+    </div>
+  )
 }
 
 export default Page
