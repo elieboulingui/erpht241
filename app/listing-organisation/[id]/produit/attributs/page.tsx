d95f6212fe5ab609c97b5284cbc7aa79622f2980
JojoELITE <jojoelite1014@gmail.com>
2025-04-15 07:54:52 +0100

    common component to optimize the TABS

--- app/listing-organisation/[id]/produit/attributs/page.tsx
+++ app/listing-organisation/[id]/produit/attributs/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React, { useState } from "react";
 import DashboardSidebar from "@/components/DashboardSidebar";
-import { AttributsHeader } from "./components/AttributsHeader";
 import TabsMenu from "./components/tabsMenu";
 
 export default function Page() {
@@ -12,7 +11,6 @@ export default function Page() {
         <DashboardSidebar />
       </div>
       <div className="w-full">
-    
         <TabsMenu/>
       </div>
     </div>
