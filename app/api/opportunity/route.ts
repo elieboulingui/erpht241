7c8207e0e7b2191c5b16ee062fc95f9a41a58372
unknown <elieboulingui2@gmail.com>
2025-05-08 11:10:40 +0200

    update update column

--- app/api/opportunity/route.ts
+++ app/api/opportunity/route.ts
@@ -1,25 +1,22 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-// Le paramètre `stepId` est un exemple si tu veux récupérer les opportunités pour une étape spécifique
 export async function GET(request: Request) {
   try {
-    // Récupération du paramètre `stepId` depuis les URL ou les query parameters
     const url = new URL(request.url);
     const stepId = url.searchParams.get("stepId");
 
-    // Requête pour récupérer les opportunités avec une possible inclusion d'une étape spécifique
     const opportunities = await prisma.opportunity.findMany({
-      where: stepId ? { stepId: stepId } : {}, // Filtre les opportunités si un stepId est fourni
+      where: stepId ? { stepId: stepId } : {},
       include: {
-        step: true, // Inclut les données liées à l'étape
+        step: true,
+        contact: true, // On inclut les données du contact ici
       },
       orderBy: {
-        createdAt: "asc", // Trie les opportunités par date de création (si nécessaire)
+        createdAt: "asc",
       },
     });
 
-    // Vérifier si des opportunités ont été trouvées
     if (opportunities.length === 0) {
       return NextResponse.json(
         { message: "Aucune opportunité trouvée." },
@@ -27,11 +24,34 @@ export async function GET(request: Request) {
       );
     }
 
-    return NextResponse.json(opportunities, { status: 200 });
+    // Récupérer uniquement les contacts associés à chaque opportunité
+    const contacts = opportunities 
+      .filter((opp) => opp.contact !== null) // Assure-toi qu'il y a bien un contact
+      .map((opp) => ({
+        id: opp.contact.id,
+        name: opp.contact.name,
+        logo: opp.contact.logo,
+        adresse: opp.contact.adresse,
+        status_contact: opp.contact.status_contact,
+        email: opp.contact.email,
+        phone: opp.contact.phone,
+        niveau: opp.contact.niveau,
+        tags: opp.contact.tags,
+        sector: opp.contact.sector,
+        createdAt: opp.contact.createdAt,
+        updatedAt: opp.contact.updatedAt,
+        isArchived: opp.contact.isArchived,
+        archivedAt: opp.contact.archivedAt,
+        archivedBy: opp.contact.archivedBy,
+        createdByUserId: opp.contact.createdByUserId,
+        updatedByUserId: opp.contact.updatedByUserId,
+      }));
+    console.log(contacts)
+    return NextResponse.json(contacts, { status: 200 });
   } catch (error) {
-    console.error("Erreur lors de la récupération des opportunités :", error);
+    console.error("Erreur lors de la récupération des contacts :", error);
     return NextResponse.json(
-      { error: "Erreur serveur lors de la récupération des opportunités." },
+      { error: "Erreur serveur lors de la récupération des contacts." },
       { status: 500 }
     );
   }
